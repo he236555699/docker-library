78013f1a85a6c3b2917a751762ca895dc91388fb
zhoufei <he236555699@163.com>
2018-01-15 18:42:16 +0800

    update slave docker entrypoint shell script

--- mysql/5.7/slave/tardition/docker-entrypoint.sh
+++ mysql/5.7/slave/tardition/docker-entrypoint.sh
@@ -179,6 +179,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		if [ "$MYSQL_REPLICATION_USER" -a "$MYSQL_REPLICATION_PASSWORD" ]; then
 			if ["$MYSQL_MASTER_SERVICE_NAME"]; then
 				echo "STOP SLAVE;" | "${mysql[@]}" 
+				echo "START SLAVE UNTIL SQL_AFTER_MTS_GAPS;" | "${mysql[@]}" 
 				echo "CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_SERVICE_NAME', MASTER_USER='$MYSQL_REPLICATION_USER', MASTER_PASSWORD='$MYSQL_REPLICATION_PASSWORD';" | "${mysql[@]}" 
 				echo "CHANGE MASTER TO MASTER_HEARTBEAT_PERIOD = 10;" | "${mysql[@]}"
 				echo "START SLAVE;" | "${mysql[@]}"
